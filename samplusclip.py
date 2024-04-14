ef620f0a54f8ec39d59e2eb1974eb94ffaf8f697
PSRahul <psrahul.ml@gmail.com>
2024-04-14 19:41:57 +0200

    multiclass works

--- samplusclip.py
+++ samplusclip.py
@@ -16,57 +16,59 @@ from PIL import Image
 from samplusclip_utils import *
 
 
-
-@hydra.main(version_base=None,config_path="conf", config_name="config.yaml")
-def main(cfg : DictConfig):
+@hydra.main(version_base=None, config_path="conf", config_name="config.yaml")
+def main(cfg: DictConfig):
 
     print(OmegaConf.to_yaml(cfg))
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    #device="cpu"
-    #sam = sam_model_registry["vit_h"](checkpoint= "model-weights/sam_vit_h_4b8939.pth").to(device)
-   
-    sam = sam_model_registry["vit_b"](checkpoint= "model-weights/sam_vit_b_01ec64.pth").to(device)
-    mask_generator = SamAutomaticMaskGenerator(sam)
-    
+    # device="cpu"
+    # sam = sam_model_registry["vit_h"](checkpoint= "model-weights/sam_vit_h_4b8939.pth").to(device)
 
-    input_images=os.listdir(cfg.image.input_folder)
+    sam = sam_model_registry["vit_b"](
+        checkpoint="model-weights/sam_vit_b_01ec64.pth"
+    ).to(device)
+
+    input_images = os.listdir(cfg.image.input_folder)
     for each_input_image in input_images:
-            image_path =os.path.join(cfg.image.input_folder,each_input_image)
-            image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  
-            image = adjust_image_size(image)
-            masks = mask_generator.generate(image)
-            output_dict={}
-            output_mask=[]
+        image_path = os.path.join(cfg.image.input_folder, each_input_image)
 
-            #dummy_image = draw_masks(image, masks)
-            #dummy_image = PIL.Image.fromarray(dummy_image)
-            #dummy_image.show()
-            for each_class in cfg.product_names:
+        image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        mask_generator = SamAutomaticMaskGenerator(
+            sam, min_mask_region_area=0.25 * image.shape[0] * image.shape[1]
+        )
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = adjust_image_size(image)
+        masks = mask_generator.generate(image)
+        output_dict = {}
+        output_mask = []
 
-            
-                query,masks = filter_masks(
+        # dummy_image = draw_masks(image, masks)
+        # dummy_image = PIL.Image.fromarray(dummy_image)
+        # dummy_image.show()
+        for each_class in cfg.product_names:
+
+            query_dict, masks = filter_masks(
                 image,
                 masks,
                 predicted_iou_threshold=0.9,
                 stability_score_threshold=0.8,
-                query=each_class,
+                query=cfg.product_names,
                 clip_threshold=0.85,
             )
-                if(len(masks)):
-                    output_dict[query]=masks
-                    output_mask.extend(masks)
+            if len(masks):
+                # output_dict[query] = masks
+                output_mask.extend(masks)
 
-            image = draw_masks(image, output_mask)
-            print(each_input_image)
-            for key,value in output_dict.items():
-                 print(each_input_image,key,len(value))
-            #image = PIL.Image.fromarray(image)
-            #image.show()
-            pass
+        image = draw_masks(image, output_mask)
+        print(each_input_image)
+        for key, value in query_dict.items():
+            print(each_input_image, key, len(value))
+        image = PIL.Image.fromarray(image)
+        image.show()
+        pass
 
-    
+
 if __name__ == "__main__":
 
     main()
--- samplusclip_utils.py
+++ samplusclip_utils.py
@@ -10,7 +10,10 @@ import numpy as np
 import PIL
 import torch
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+from collections import defaultdict
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 @lru_cache
 def load_clip(
@@ -18,6 +21,7 @@ def load_clip(
 ) -> Tuple[torch.nn.Module, Callable[[PIL.Image.Image], torch.Tensor]]:
     model, preprocess = clip.load(name, device=device)
     return model.to(device), preprocess
+
 
 TOP_K_OBJ = 10
 
@@ -30,7 +34,8 @@ def get_score(crop: PIL.Image.Image, texts: List[str]) -> torch.Tensor:
     tokens = clip.tokenize(texts).to(device)
     logits_per_image, _ = model(preprocessed, tokens)
     similarity = logits_per_image.softmax(-1).cpu()
-    return similarity[0, 0]
+    return int(torch.argmax(similarity[0])), torch.max(similarity[0])
+
 
 def crop_image(image: np.ndarray, mask: Dict[str, Any]) -> PIL.Image.Image:
     mask["bbox"] = list(map(int, mask["bbox"]))
@@ -55,7 +60,9 @@ def crop_image(image: np.ndarray, mask: Dict[str, Any]) -> PIL.Image.Image:
     crop = PIL.Image.fromarray(crop)
     return crop
 
+
 MAX_WIDTH = MAX_HEIGHT = 1024
+
 
 def adjust_image_size(image: np.ndarray) -> np.ndarray:
     height, width = image.shape[:2]
@@ -68,8 +75,14 @@ def adjust_image_size(image: np.ndarray) -> np.ndarray:
     image = cv2.resize(image, (width, height))
     return image
 
+
 def get_texts(query: str) -> List[str]:
+    prompts = []
+    prompts.extend(f"a picture of {each_query}" for each_query in query)
+    prompts.extend("a picture of background")
+    return prompts
     return [f"a picture of {query}", "a picture of background"]
+
 
 def filter_masks(
     image: np.ndarray,
@@ -79,19 +92,25 @@ def filter_masks(
     query: str,
     clip_threshold: float,
 ) -> List[Dict[str, Any]]:
-    filtered_masks: List[Dict[str, Any]] = []
 
+    filtered_masks = []
+    filtered_query = defaultdict(list)
     for mask in sorted(masks, key=lambda mask: mask["area"])[-TOP_K_OBJ:]:
         if (
             mask["predicted_iou"] > predicted_iou_threshold
             and mask["stability_score"] > stability_score_threshold
             and image.shape[:2] == mask["segmentation"].shape[:2]
-            and query):
-            
-            if(get_score(crop_image(image, mask), get_texts(query)) > clip_threshold):            
-                filtered_masks.append(mask)
+            and query
+            and mask["area"] != image.shape[0] * image.shape[1]
+        ):
+            class_idx, score = get_score(crop_image(image, mask), get_texts(query))
 
-    return query,filtered_masks
+            if score > clip_threshold:
+                filtered_masks.append(mask)
+                filtered_query[query[class_idx]].append(mask)
+
+    return filtered_query, filtered_masks
+
 
 def draw_masks(
     image: np.ndarray, masks: List[np.ndarray], alpha: float = 0.7
